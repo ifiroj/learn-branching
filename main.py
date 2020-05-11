c38f65c7a7475712038dc64a8e2454bda09b31a7
ifiroj <firoj.acharya@gmail.com>
2020-05-11 20:58:02 +0545

    finishing main and body

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
 def adding():
     print("adding")
+
 def displaying():
     print("displaying")
+
 def searching():
     print("searching")
+
 def updating():
     print("updating")
+
 def deleting():
     print("Deleted")
-
-while True:
-    print("Select an option")
-    print("1. Add")
-    print("2. Show")
-    print("3. Search")
-    print("4. Update")
-    print("5. Delete")
-    print("6. Exit")
-    choice = int(input())
-
-    if choice==1:
-        print("Adding")
-    elif choice==2:
-        print("Showing")
-    elif choice==3:
-        print("Searching")
-    elif choice==4:
-        print("Updating")
-    elif choice==5:
-        print("Deleting")
-    elif choice==6:
-        break
-    else:
-        print("Invalid Option")
