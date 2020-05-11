f64699a3a2f8d9f780a47cb2e03b67bdc5d7f51d
ifiroj <firoj.acharya@gmail.com>
2020-05-11 20:02:55 +0545

    making a small project

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("I am trying to use branch")
+while True:
+    print("Select an option")
+    print("1. Add")
+    print("2. Show")
+    print("3. Search")
+    print("4. Update")
+    print("5. Delete")
+    print("6. Exit")
+    choice = int(input())
+
+    if choice==1:
+        print("Adding")
+    elif choice==2:
+        print("Showing")
+    elif choice==3:
+        print("Searching")
+    elif choice==4:
+        print("Updating")
+    elif choice==5:
+        print("Deleting")
+    elif choice==6:
+        break
+    else:
+        print("Invalid Option")
